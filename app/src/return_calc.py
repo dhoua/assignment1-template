8cd4a9589e4eab3c7ce43a89ce924b61249d0f27
dhoua <95493492+dhoua@users.noreply.github.com>
2022-02-27 23:44:29 -0600

    Update return_calc.py

--- app/src/return_calc.py
+++ app/src/return_calc.py
@@ -1,15 +1,18 @@
-def calculate_return(portfolios):
-    '''
-        Given a list of portfolio objects return a
-        dictionary from a portfolio ID to average stock returns 
-        in the portfolio
-    '''
-    pass
+from src.Classes.Portfolio import portfolio
+from src.Classes.Stocks import stock
+
+def avg_by_portfolio(portfolios):
+        avg_by_portfolio = {}
+        for portfolio in portfolios:
+            average = avg_return(portfolio.Stocks)
+            if avg_by_portfolio.get(portfolio.ID) is None:
+                avg_by_portfolio[portfolio.ID] = average
+        return avg_by_portfolio
 
 
 def avg_return(stocks):
-    '''
-        Given a list of stocks return the average return 
-        return 0.0  if the list is empty
-    '''
-    pass
+        if len(stocks) == 0:
+            total_return = 0
+        for stock in stocks:
+            total_return = total_return + stock.rate_of_return
+        return total_return / len(stocks)
