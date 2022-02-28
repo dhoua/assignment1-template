68d62d5251c562269f65333370b3bb1bfee2c1d5
dhoua <95493492+dhoua@users.noreply.github.com>
2022-02-27 23:43:13 -0600

    Update Portfolio.py

--- app/src/Portfolio.py
+++ app/src/Portfolio.py
@@ -1,4 +1,6 @@
-#Create a portfolio class that has the following three attributes:
-#1. ID: unique numerical identifier of the porfoltio
-#2. Description: description of the porfoltio (e.g., Finance/Real Estate/Tech)
-#3. Stocks: List of stocks in the portfolio
+class portfolio():
+    def __init__(self, ID, Description, Stocks):
+        self.ID = ID
+        self.Description = Description
+        self.Stocks = Stocks
+        
